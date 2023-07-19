b859b8f351082b0bf72205e2fcde713fb5d0fb67
Reda Mansy <mansyr@cardiff.ac.uk>
2023-07-19 16:11:06 +0100

    added edit readme

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import datetime
 from datetime import date
+import enum
+import re
 
 def delta_days():
-    
+
     date_started = datetime.date(2018, 7, 9)
     today = date.today()
 
     delta = today - date_started
 
     return delta.days
+
+days = delta_days()
+
+def edit_readme(days):
+    with open("README.md") as readmefile:
+        data = readmefile.readlines()
+
+    # print(data[11])
+    data[11] = f"⚡ Fun fact **I've been journaling every day for {days} days straight**\n"
+
+    with open("README.md", "w") as readmefile:
+        readmefile.writelines(data)
+
+edit_readme(days)
+
+
+
+
